17b8c9e610f8cfcb348d034f0a277b5d628b9d24
Rsiesener <r.siesener91@gmail.com>
2022-04-15 14:29:29 -0500

    initial commit

---
+++ config/connect.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const connectDb = async() => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    })
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
+  }
+}
+
+module.exports = connectDb
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+
+const connectDb = require('./config/connect.js')
+
+const app = express()
+const port = 8888
+
+dotenv.config()
+
+app.use(express.json()) // Parses JSON dated from post/put requests
+app.use(cors()) // allows front and backend to talk to each other if they are on different servers
+
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`)
+})
